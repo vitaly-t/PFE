62de3f041d0494d822237fb9ac7b2588e814cccf
llalanne <lucielalanne@hotmail.fr>
2017-01-11 17:50:49 +0100

    visualisation des donnees sur graphs

--- node-postgres-promises/queries.js
+++ node-postgres-promises/queries.js
@@ -201,18 +201,73 @@ function graphiques(req, res, next) {
   , ctx = canvas.getContext('2d')
   , Chart = require('nchart')
   , fs = require('fs')
-  , data = require('./line.json')
 
-ctx.fillStyle = '#fff';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-new Chart(ctx).Line(data);
+var datatemps = [], datax = [], datay = [], dataz = [];
 
-  canvas.toBuffer(function (err, buf) {
-    if (err) throw err;
-    fs.writeFile(__dirname + '/public'+'/images'+'/pie.png', buf);
-  });
-  res.render('graphiques', {title: 'LUL'});
-}
+  db.any({
+    name: "graphs",
+    text: "select * from donnees where id_deploiement = 2"
+  })
+    .then(function (data) {
+      for (var i = 0; i < 6; i++){
+        datatemps[i]=data[i].temps
+        datax[i]=data[i].x;
+        datay[i]=data[i].y;
+        dataz[i]=data[i].z
+      }
+      console.log('Dans callback')
+      console.log(datatemps);
+      console.log(datax);
+
+      var lineChartData = {
+      	labels : datatemps,
+        scaleLabel : "<%=value%>",
+      	datasets : [
+      		{
+      			label: "My First dataset",
+      			fillColor : "rgba(220,220,220,0.2)",
+      			strokeColor : "rgba(220,220,220,1)",
+      			pointColor : "rgba(220,220,220,1)",
+      			pointStrokeColor : "#fff",
+      			pointHighlightFill : "#fff",
+      			pointHighlightStroke : "rgba(220,220,220,1)",
+      			data : datax
+      		},
+      		{
+      			label: "My Second dataset",
+      			fillColor : "rgba(151,187,205,0.2)",
+      			strokeColor : "rgba(151,187,205,1)",
+      			pointColor : "rgba(151,187,205,1)",
+      			pointStrokeColor : "#fff",
+      			pointHighlightFill : "#fff",
+      			pointHighlightStroke : "rgba(151,187,205,1)",
+      			data : datay
+      		}
+      	]
+
+
+      }
+
+
+
+      ctx.fillStyle = '#fff';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      new Chart(ctx).Line(lineChartData);
+
+      canvas.toBuffer(function (err, buf) {
+        if (err) throw err;
+        fs.writeFile(__dirname + '/public'+'/images'+'/pie.png', buf);
+      });
+      res.render('graphiques', {title: 'LUL'});
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  }
+
+
+
+
 
 
 module.exports = {
